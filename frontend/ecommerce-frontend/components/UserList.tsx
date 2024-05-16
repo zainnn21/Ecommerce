4bc71c37d62ad2bd6e58a02fc5a4b2c6ad128510
Zain <abibz97@gmail.com>
2024-05-16 13:03:30 +0700

    init frontend

---
+++ frontend/ecommerce-frontend/components/UserList.tsx
@@ -0,0 +1,23 @@
+import {FC} from 'react';
+import {User} from '../types';
+
+interface UserListProps {
+    users: User[];
+}
+
+const UserList: FC<UserListProps> = ({users}) => {
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {users.map((user) => (
+                    <li> key={user.id}>
+                        {user.name} - {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default UserList;
